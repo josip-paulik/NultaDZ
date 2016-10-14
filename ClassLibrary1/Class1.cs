5e7211b97f20e0204c7aa15e29df0e494b6f41bf
josip-paulik <josip.paulik@outlook.com>
2016-10-14 19:20:20 +0200

    Add project files.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class MyClass
+{
+        public static void MyPrintHelloWorld()
+        {
+            Console.WriteLine("My Hello World Print.HAHAHAH!");
+        }
+}
+
